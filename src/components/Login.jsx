c4fa2442ff6df244a7444b71ffc9b364d3e7ef9f
CreepyPasta69 <146427822+CreepyPasta69@users.noreply.github.com>
2024-08-04 18:45:13 +0530

    Adding image via import

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import google from "../assets/google.png"
+
 import "./Login.css"
 
 export default function Login(props){
@@ -7,7 +9,7 @@ export default function Login(props){
         <div className="login">
             <h1>Hello User</h1>
             <p>Login required to continue to the app</p>
-            <button onClick={props.login}><img src="src/assets/google.png" alt="" /><p>Login</p></button>
+            <button onClick={props.login}><img src={google} alt="" /><p>Login</p></button>
         </div>
     )
 }
