12a58d219777dd4c8db7f857d1ad8a42ea2e8f29
CreepyPasta69 <146427822+CreepyPasta69@users.noreply.github.com>
2024-08-07 01:59:12 +0530

    Functional Sidebar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../firebase.js";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 import SideBar from "./SideBar";
 import Login from "./Login.jsx";
+import Home from "./Home.jsx";
 import ChatBox from "./ChatBox";
+import Friends from "./Friends.jsx";
 
 import "./App.css";
 
 export default function App() {
   const [user] = useAuthState(auth);
+  const [activeMenu, setActiveMenu] = useState("Home")
 
   const googleSignin = () => {
     const provider = new GoogleAuthProvider();
@@ -27,8 +30,10 @@ export default function App() {
         <Login login={googleSignin} />
       ) : (
         <div className="app">
-          <SideBar logout={signOut}/>
-          <ChatBox />
+          <SideBar activeMenu={activeMenu} setActiveMenu={setActiveMenu} logout={signOut}/>
+          {activeMenu === "Home" && <Home/>}
+          {activeMenu === "Chat" && <ChatBox />}
+          {activeMenu === "Friends" && <Friends />}
         </div>
       )}
     </>
---
+++ src/components/Friends.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Friends.css"
+
+export default function Friends(){
+    return(
+        <div className="friends">
+            <h1>Really?</h1>
+        </div>
+    )
+}
